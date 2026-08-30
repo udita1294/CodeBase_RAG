0606c7a396ae2b07546a5beda044f9f9c6fdb2fe
Udita Singh <udita1294@gmail.com>
2026-08-30 14:56:37 +0530

    basic setup of main.py done

---
+++ backend/main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+
+app = FastAPI(title="Codebase RAG", description= "AI Software Engineer for understanding GitHub repositories", version= "0.1.0")
+
+@app.get("/")
+def root():
+    return {"message": "Welcome to Codebase RAG!"}
+
+@app.get("/health")
+def health_check():
+    return {"status": "healthy"}
+
+
--- main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from codebase-rag!")
-
-
-if __name__ == "__main__":
-    main()
